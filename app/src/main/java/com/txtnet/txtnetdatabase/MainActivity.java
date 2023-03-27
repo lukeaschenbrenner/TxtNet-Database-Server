2354f77c47d234c2ad61185541ab253dfd45eb2a
Luke Aschenbrenner <ashbren21@gmail.com>
2023-03-26 19:47:07 -0500

    Commit

--- app/src/main/java/com/txtnet/txtnetdatabase/MainActivity.java
+++ app/src/main/java/com/txtnet/txtnetdatabase/MainActivity.java
@@ -12,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //central database app to accept txtnet-related queries
         //runs as a service, separate from txtnet browser
-        
+
         //example incoming text to handle from a txtnet server: register country-code 1 [recipient number] server
 
         //example outgoing sms to txtnet server: ping TxtNet Server vLATEST
         //example incoming response text to handle from a txtnet server: TxtNet Server v54 alive (possibly include requests per hour?)
+        // run periodic ping checks. after a certain timeout, assume server is dead and remove from database.
+
 
         //example incoming text to handle from a txtnet client: list servers region 1
         /*
